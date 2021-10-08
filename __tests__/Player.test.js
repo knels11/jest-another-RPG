e0a07c7964739b01162e836a97818d59d41d7ba7
kiehp <kierra.nelson@aol.com>
2021-10-08 16:03:26 -0400

    mock tests passed

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,11 @@
 const Player = require('../.lib/Player');
+//imports Potion() constructor into test, establishing Potion as a usable variable
+const Potion = require('../.lib/Potion');
+
+//mocks/replaces the constructor's implementation with our faked data
+jest.mock('../.lib/Potion');
+console.log(new Potion());
+
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -7,4 +14,8 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
 });
